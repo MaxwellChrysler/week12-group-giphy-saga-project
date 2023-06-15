e666707b6d67b3dc11e255ffc61394838282d22f
Maxwell Chrysler <maxwellchrysler@hotmail.com>
2023-06-15 14:36:50 -0500

    https://

---
+++ src/components/Favorites/FavoritesItem.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import axios from "axios";
+
+function FavoritesItem(){
+
+    const dispatch = useDispatch();
+
+
+  
+    const renderFavorites = () => {
+            dispatch({ type: FETCH_IMAGES, payload: {} });
+          };
+
+
+    return(
+
+        <h1>hellow world</h1>
+
+        
+    )
+
+
+
+
+}
+
+export default FavoritesItem;
--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -13,6 +13,7 @@ function HomePage() {
 
     const apiSearch = (event) => {
         event.preventDefault();
+        console.log('Search is:' , search)
         dispatch({ type: 'FETCH_API', payload: search });
     }
 
@@ -23,14 +24,14 @@ function HomePage() {
     return (
         <>
             <h1>Home Page</h1>
-            <form>
+            <form onSubmit={apiSearch }>
                 <input
                     required
                     type="text"
                     value={search}
                     onChange={(event) => setSearch(event.target.value)}
                 />
-                <button onSubmit={apiSearch}>Search</button>
+                <button type='submit'>Search</button>
             </form>
             {gifs.map((gif, i) => (
                 <div key={i}>
