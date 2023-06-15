9897156839862cb6721472d50ea883a12dbe1b0d
Dave Nash <davidjnash2@gmail.com>
2023-06-15 17:10:59 -0500

    built serverside get

--- server/routes/favorite.router.js
+++ server/routes/favorite.router.js
@@ -5,7 +5,19 @@ const router = express.Router();
 
 // return all favorite images
 router.get("/", (req, res) => {
-  res.sendStatus(200);
+  const queryText = `
+    SELECT "favorites"."id", "favorites"."url", "category"."name"
+    FROM "favorites" 
+    JOIN "category"
+    ON "favorites"."category_id" = "category"."id";`;
+    pool.query(queryText)
+  .then((response) => { 
+    res.send(response.rows);
+    res.sendStatus(200); 
+  }) 
+  .catch((error) => {
+    console.log('error in server favorites GET', error);
+  })
 });
 
 // add a new favorite
