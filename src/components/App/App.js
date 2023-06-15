9b36930402d06dd5f582433b48dc40b4529d9ea1
Conrad Rolf <williamconradrolf@gmail.com>
2023-06-15 10:30:35 -0500

    added in folders and files and linked them to the App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Favorites from '../Favorites/Favorites';
+import HomePage from '../HomePage/HomePage';
 
 function App(props) {
   return (
     <div>
       <h1>Giphy Search!</h1>
+      <HomePage />
+      <Favorites />
     </div>
   );
 }
